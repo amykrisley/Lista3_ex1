187ca2fac966907d359d5f2da2e35fdcbfba58e1
amykrisley <130614797+amykrisley@users.noreply.github.com>
2023-05-08 09:51:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            do
+            {
+                Console.Write("Digite um valor positivo: ");
+                a = int.Parse(Console.ReadLine());
+            }
+            while (a <= 0);
+        }
+    }
+}
